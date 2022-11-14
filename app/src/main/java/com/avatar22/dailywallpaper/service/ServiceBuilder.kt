29563820b4f7df80d86abc1c32e8b1d9670a839a
AVATAR2222 <alireza.abbasi1997@gmail.com>
2022-11-14 11:01:59 +0330

    gif

--- app/src/main/java/com/avatar22/dailywallpaper/service/ServiceBuilder.kt
+++ app/src/main/java/com/avatar22/dailywallpaper/service/ServiceBuilder.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceBuilder {
 
-    private const val Url="http://192.168.1.5/Laravel-Wallpaper-Api/public/api/"
+    private const val Url="http://192.168.1.4/Laravel-Wallpaper-Api/public/api/"
 
     // Create OkHttp Client
     private val okHttp = OkHttpClient.Builder()
